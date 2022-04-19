74bb3c48530e4aecf0b1003d976fce78669f70c2
Ricardo Sampaio <ricardo_cs@outlook.pt>
2022-04-19 17:52:28 +0100

    Ao criar uma boleia, o utilizador tem de escolher um dos polos do ipca.

--- app/src/main/java/com/example/ipcaboleias/createPublication/CreatePublicationSearchEndLocationFragment.kt
+++ app/src/main/java/com/example/ipcaboleias/createPublication/CreatePublicationSearchEndLocationFragment.kt
@@ -5,8 +5,7 @@ import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
-import android.widget.EditText
-import android.widget.Toast
+import android.widget.*
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.activityViewModels
 import com.example.ipcaboleias.R
@@ -27,16 +26,17 @@ import java.net.HttpURLConnection
 import java.net.URL
 
 
-class CreatePublicationSearchEndLocationFragment : Fragment(R.layout.fragment_create_publication_search_end_location), OnMapReadyCallback {
+class CreatePublicationSearchEndLocationFragment :
+    Fragment(R.layout.fragment_create_publication_search_end_location), OnMapReadyCallback {
 
-    private var _binding : FragmentCreatePublicationSearchEndLocationBinding? = null
+    private var _binding: FragmentCreatePublicationSearchEndLocationBinding? = null
     private val binding get() = _binding!!
 
     private val CREATE_PUB_SEARCH2_FRAG_TAG = "createPubSearch2FragTag"
     private val CREATE_PUB_PICK_DATE_FRAG_TAG = "createPubPickDateFragTag"
 
-    private var latitude : Double = 0.0
-    private var longitude : Double = 0.0
+    private var latitude: Double = 0.0
+    private var longitude: Double = 0.0
 
     private var map: GoogleMap? = null
 
@@ -46,33 +46,63 @@ class CreatePublicationSearchEndLocationFragment : Fragment(R.layout.fragment_cr
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         _binding = FragmentCreatePublicationSearchEndLocationBinding.bind(view)
 
+
         val supportFragmentManager = requireActivity().supportFragmentManager
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        val spinner = requireActivity().findViewById<Spinner>(R.id.spinnerSearchLocation)
+
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.IPCAPolos,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
 
         mapFragment.getMapAsync(this)
 
         binding.apply {
-            buttonSearch.setOnClickListener {
-                if(etSearchLocation.text.toString() == ""){
-                    Toast.makeText(requireContext(), "Sem informações para procurar", Toast.LENGTH_SHORT).show()
+
+
+            spinnerSearchLocation.onItemSelectedListener =
+                object : AdapterView.OnItemSelectedListener {
+                    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+
+                        lateinit var latLng: LatLng
+
+                        when (p2) {
+                            0 -> latLng = LatLng(41.536587, -8.627911)
+                            1 -> latLng = LatLng(41.542142, -8.421324)
+                            2 -> latLng = LatLng(41.507823, -8.335278)
+                            3 -> latLng = LatLng(41.440063, -8.477200)
+                        }
+                        changeMapLocationLatLng(latLng)
+                    }
+
+                    override fun onNothingSelected(p0: AdapterView<*>?) {
+                        TODO("Not yet implemented")
+                    }
                 }
-                searchLocation(etSearchLocation.text.toString())
-            }
+
 
             btnNext.setOnClickListener {
                 model.setEndLatitude(latitude)
                 model.setEndLongitude(longitude)
 
-                val fragToHide = supportFragmentManager.findFragmentByTag(CREATE_PUB_SEARCH2_FRAG_TAG)
-                val fragToCall = supportFragmentManager.findFragmentByTag(CREATE_PUB_PICK_DATE_FRAG_TAG)
+                val fragToHide =
+                    supportFragmentManager.findFragmentByTag(CREATE_PUB_SEARCH2_FRAG_TAG)
+                val fragToCall =
+                    supportFragmentManager.findFragmentByTag(CREATE_PUB_PICK_DATE_FRAG_TAG)
 
-                if(fragToCall != null){
+                if (fragToCall != null) {
                     supportFragmentManager.beginTransaction().show(fragToCall).commit()
-                }
-                else{
+                } else {
                     supportFragmentManager.beginTransaction().add(
                         R.id.frameLayoutFilter,
-                        CreatePublicationPickDateFragment.newInstance(), CREATE_PUB_PICK_DATE_FRAG_TAG).commit()
+                        CreatePublicationPickDateFragment.newInstance(),
+                        CREATE_PUB_PICK_DATE_FRAG_TAG
+                    ).commit()
                 }
 
                 supportFragmentManager.beginTransaction().hide(fragToHide!!).commit()
@@ -84,71 +114,25 @@ class CreatePublicationSearchEndLocationFragment : Fragment(R.layout.fragment_cr
         @JvmStatic
         fun newInstance() =
             CreatePublicationSearchEndLocationFragment().apply {
-                }
             }
+    }
 
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onMapReady(p0: GoogleMap) {
         map = p0
 
-        val latLng : LatLng = LatLng(41.536587, -8.627911)
+        val latLng: LatLng = LatLng(41.536587, -8.627911)
 
         map!!.addMarker(MarkerOptions().position(latLng))
         map!!.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16.0f))
 
-        map!!.setOnMapClickListener {
-            latitude = it.latitude
-            longitude = it.longitude
-
-            map!!.clear()
-
-            map!!.addMarker(
-                MarkerOptions().position(it)
-            )
-            map!!.animateCamera(CameraUpdateFactory.newLatLngZoom(it, 16.0f))
-
-            // Change location in editText
-            reverseGeocoding(it.latitude, it.longitude)
-        }
     }
 
-    @RequiresApi(Build.VERSION_CODES.N)
-    private fun reverseGeocoding(lat : Double, long : Double){
-        GlobalScope.launch(Dispatchers.IO) {
-            val url =  URL("https://nominatim.openstreetmap.org/reverse?format=json&lat=$lat&lon=$long&zoom=18")
-
-            with(url.openConnection() as HttpURLConnection) {
-                requestMethod = "GET"  // optional default is GET
-
-                println("\nSent 'GET' request to URL : $url; Response Code : $responseCode")
-
-                inputStream.bufferedReader().use {
-                    val json = it.readText()
-                    val topic = Gson().fromJson(json, ReverseGeo::class.java)
-
-                    launch(Dispatchers.Main) {
-                        val et = requireActivity().findViewById<EditText>(R.id.etSearchLocation)
-                        et.setText(topic.address.county)
-                    }
-                }
-            }
-        }
-    }
-
-    @RequiresApi(Build.VERSION_CODES.N)
-    private fun searchLocation(location : String){
-        val geoCoder = Geocoder(activity)
-        val address = geoCoder.getFromLocationName(location, 1)
-
-        val coordinates = LatLng(address[0].latitude, address[0].longitude)
-
+    private fun changeMapLocationLatLng(latLng: LatLng) {
         map!!.clear()
 
-        map!!.addMarker(MarkerOptions().position(coordinates))
-        map!!.animateCamera(CameraUpdateFactory.newLatLngZoom(coordinates, 16.0f))
-
-        latitude = coordinates.latitude
-        longitude = coordinates.longitude
-        reverseGeocoding(coordinates.latitude, coordinates.longitude)
+        map!!.addMarker(MarkerOptions().position(latLng))
+        map!!.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16.0f))
     }
+
 }
